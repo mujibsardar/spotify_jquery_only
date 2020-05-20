139504e7e87613189dcaf57544e4580deb85e81a
Avan <mujibsardar@gmail.com>
2020-05-20 11:47:50 -0700

    authorize.html

--- authorize.js
+++ authorize.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
   console.log( "authorize.js ready!" );
+  console.log('version 1');
   let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
   let redirect_uri = 'https://mujibsardar.github.io/spotify_jquery_only';
   const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
