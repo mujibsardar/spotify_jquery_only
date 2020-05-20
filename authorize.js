af65c1e360ff49255f1342db3cfdc9cd6a1825e5
Avan <mujibsardar@gmail.com>
2020-05-20 11:43:42 -0700

    another commit

--- authorize.js
+++ authorize.js
@@ -1,4 +1,8 @@
-let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
-let redirect_uri = ;
-const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
-// window.location.replace(redirect);
+$( document ).ready(function() {
+  console.log( "authorize.js ready!" );
+  let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
+  let redirect_uri = ;
+  const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
+  // window.location.replace(redirect);
+  
+}
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-    
+    console.log( "script.js ready!" );
+
 });
