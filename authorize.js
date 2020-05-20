5807784c7da655710a7de12d091e8a2f9760eefe
Avan <mujibsardar@gmail.com>
2020-05-20 11:53:46 -0700

    authorize.html fix url encode

--- authorize.js
+++ authorize.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
   console.log( "authorize.js ready!" );
-  console.log('version 2');
+  console.log('version 3');
   let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
-  let redirect_uri = 'https://mujibsardar.github.io/spotify_jquery_only';
+  let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only';
   const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
   window.location.replace(redirect);
 });
