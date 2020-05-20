cbadb4e1ef4e0c001c1134893220d216d6988e0b
Avan <mujibsardar@gmail.com>
2020-05-20 12:20:04 -0700

    update script.js embed song

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     console.log( 'script.js ready!' );
-    console.log('version 1')
+    console.log('version 2')
     /////https://stackoverflow.com/questions/2988050/html5-audio-player-jquery-toggle-click-play-pause
     const getUrlParameter = (sParam) => {
       let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
@@ -21,7 +21,7 @@ $( document ).ready(function() {
   console.log(`accessToken ${accessToken}`);
 
   $.ajax ({
-      url: 'https://api.spotify.com/v1/search?q=kevin&type=track',
+      url: 'https://api.spotify.com/v1/search?q=dance+off&type=track',
       type: 'GET',
       headers: {
           'Authorization' : 'Bearer ' + accessToken
@@ -36,6 +36,12 @@ $( document ).ready(function() {
         let id = data.tracks.items[0].id;
         console.log(' ');
         console.log(`id ${id}`); ////id 1TEL6MlSSVLSdhOSddidlJ
+
+        let src_str = `https://open.spotify.com/embed/track/${id}`;
+        console.log(`src_str ${src_str}`);
+        let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+        let parent_div = $('#content');
+        parent_div.append(iframe);
       }
   });
 
