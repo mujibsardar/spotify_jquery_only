c9f3526d985c7301cdeb65115fa667fc538b1ff7
Avan <mujibsardar@gmail.com>
2020-05-20 16:54:32 -0700

    version change

--- authorize.js
+++ authorize.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
   console.log( "authorize.js ready!" );
   console.log('version 5');
-  let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
-  let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only';
+  // let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
+  // let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only';
   const redirect = `https://accounts.spotify.com/authorize?client_id=df6968c34f4b4494ac206c7628d2afc7&response_type=token&redirect_uri=https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only`;
   console.log(`redirect url: ${redirect}`);
   window.location.replace(redirect);
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     console.log( 'script.js ready!' );
-    console.log('version 2')
+    console.log('version 3')
     /////https://stackoverflow.com/questions/2988050/html5-audio-player-jquery-toggle-click-play-pause
     const getUrlParameter = (sParam) => {
       let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
@@ -17,8 +17,8 @@ $( document ).ready(function() {
       }
   };
 
-  const accessToken = getUrlParameter('access_token');
-  console.log(`accessToken ${accessToken}`);
+    const accessToken = getUrlParameter('access_token');
+    console.log(`accessToken ${accessToken}`);
 
   $.ajax ({
       url: 'https://api.spotify.com/v1/search?q=dance+off&type=track',
@@ -29,14 +29,14 @@ $( document ).ready(function() {
       success: function(data) {
         console.log(' ');
         console.log(' ');
-        console.log('Success');
+        console.log('Got data back');
         // Let's console what gets returned for our search
         console.log(JSON.stringify(data));
         // Example: Extract the id of the song from the data object
         let id = data.tracks.items[0].id;
         console.log(' ');
         console.log(`id ${id}`); ////id 1TEL6MlSSVLSdhOSddidlJ
-
+        // Constructing a iframe to embed a song
         let src_str = `https://open.spotify.com/embed/track/${id}`;
         console.log(`src_str ${src_str}`);
         let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
