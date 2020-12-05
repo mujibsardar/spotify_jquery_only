f40482935f6fd376777e0bab26f23ae6ea3f47b3
Avan <mujibsardar@gmail.com>
2020-12-04 17:07:42 -0800

    ready to test

--- authorize.js
+++ authorize.js
@@ -1,13 +1,10 @@
 $( document ).ready(function() {
-  console.log( "authorize.js ready!" );
-  console.log('version 7');
   // *************** REPLACE THESE VALUES! *************************
   let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
-  // Use the following site to convert your regular url to the encoded version: 
+  // Use the following site to convert your regular url to the encoded version:
   // https://www.url-encode-decode.com/
-  let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only';
+  let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
   // *************** END *************************
   const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
-  console.log(`redirect url: ${redirect}`);
   window.location.replace(redirect);
 });
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $( document ).ready(function() {
-    console.log( 'script.js ready!' );
-    console.log('version 6')
-
+     // Helper Function to Extract Access Token for URL
     const getUrlParameter = (sParam) => {
       let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
           sURLVariables = sPageURL.split('#'),
@@ -17,28 +15,36 @@ $( document ).ready(function() {
       }
   };
 
+    // Get Access Token
     const accessToken = getUrlParameter('access_token');
-    console.log(`accessToken ${accessToken}`);
 
-  $.ajax ({
+    // AUTHORIZE with Spotify (if needed)
+    // *************** REPLACE THESE VALUES! *************************
+    let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
+    // Use the following site to convert your regular url to the encoded version:
+    // https://www.url-encode-decode.com/
+    let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
+    // *************** END *************************
+    const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
+    // Don't authorize if we have an access token already
+    console.log("Access Token: " + accessToken);
+    if(accessToken == null || accessToken == "" || accessToken == undefined){
+      window.location.replace(redirect);
+    }
+
+    // Make Spotify API call
+    $.ajax ({
       url: 'https://api.spotify.com/v1/search?q=dance+off&type=track',
       type: 'GET',
       headers: {
           'Authorization' : 'Bearer ' + accessToken
       },
       success: function(data) {
-        console.log(' ');
-        console.log(' ');
-        console.log('Got data back');
-        // Let's console what gets returned for our search
-        console.log(JSON.stringify(data));
-        // Example: Extract the id of the song from the data object
+        // Extract the id of the FIRST song from the data object
         let id = data.tracks.items[0].id;
-        console.log(' ');
-        console.log(`id ${id}`); ////id 1TEL6MlSSVLSdhOSddidlJ
-        // Constructing a iframe to embed a song
+        // Constructing two different iframes to embed the song
         let src_str = `https://open.spotify.com/embed/track/${id}`;
-        console.log(`src_str ${src_str}`);
+        // Option 2
         let iframe = `<iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
         let parent_div = $('#content');
         parent_div.append(iframe);
