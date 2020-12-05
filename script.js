158ff9ecd0929838bb43ca781a1f607a59238951
Avan <mujibsardar@gmail.com>
2020-12-04 17:15:40 -0800

    ready to test

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
           sParameterName,
           i;
       let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
-      sURLVariables = split_str[1].length > 0 ? split_str[1].split('&') : [];
+      sURLVariables = split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
       for (i = 0; i < sURLVariables.length; i++) {
           sParameterName = sURLVariables[i].split('=');
           if (sParameterName[0] === sParam) {
